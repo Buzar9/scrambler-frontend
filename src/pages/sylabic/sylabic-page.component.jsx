d2db4d3aa9363aaac74aab42e39d2baf0fa588e4
Buzar9 <48588984+Buzar9@users.noreply.github.com>
2020-06-24 20:14:43 +0200

    Create sylabic-page.component.jsx

---
+++ src/pages/sylabic/sylabic-page.component.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import CustomCipher from "../../components/custom-cipher/custom-cipher.component";
+
+import {items} from '../../assets/info.data.json'
+
+import './sylabic-page.styles.scss'
+
+class SylabicPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            url: 'http://localhost:8080/sylabic',
+            cipher: 'sylabic'
+        };
+    }
+
+    render() {
+        const { url, cipher } = this.state;
+        return (
+            <div>
+                <h1> SYLABIC </h1>
+                {items
+                    .filter((item) => item.name === cipher)
+                    .map(({text}) =>
+                        <CustomCipher url={url} text={text}/>
+                    )}
+            </div>
+        )
+    }
+}
+
+export default SylabicPage;
