ed4670c07f4f462aff8d99beb858dfcbf4b8b8cb
Buzar9 <48588984+Buzar9@users.noreply.github.com>
2020-06-24 20:14:50 +0200

    Create mors-page.component.jsx

---
+++ src/pages/mors/mors-page.component.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import './mors-page.styles.scss'
+import MorsCipher from "../../components/mors-cipher/mors-cipher.component";
+import {items} from "../../assets/info.data.json";
+
+
+class MorsPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            cipher: 'mors'
+        };
+    }
+
+    render() {
+        const { cipher } = this.state;
+        return (
+            <div>
+                <h1> MORS </h1>
+                {items
+                    .filter((item) => item.name === cipher)
+                    .map(({text}) =>
+                        <MorsCipher text={text} />
+                    )}
+            </div>
+        );
+    }
+}
+
+export default MorsPage;
